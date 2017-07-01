72b778f18ba092122d055b8d26a40e7cf62dcaf9
Richard Monastra <rmonastra2@gmail.com>
2017-07-01 00:50:05 -0400

    revision

--- models/userDB.js
+++ models/userDB.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const urlSchema = new Schema({
-    user_name: String
+    user_name: String,
+    user_id: String,
+    exerc_desc: String,
+    exerc_dura: String,
+    exerc_date: String
 });
 
 const ModelClass = mongoose.model('userDB', urlSchema);
